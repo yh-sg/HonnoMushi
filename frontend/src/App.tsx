98cb88c92c8ef5d43fe24c3a9a4ce7bbeeaea4fe
yonghan <wangyh.92@gmail.com>
2021-05-08 14:36:14 +0800

    rough layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Container, Row} from 'react-bootstrap';
 
-function App() {
+const App:React.FC = () => {
+
+  const lettersAndNumerics = "abcdefghijklmnopqrstuvwxyz0123456789",
+        letters:String[] = lettersAndNumerics.split("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>HonnoMushi</h1>
+        <Row>
+          {letters.map((letter,index)=>(
+            <Button key={index} className="col-2 m-4 p-3" variant="outline-success">{letter}</Button>
+          ))}
+        </Row>
+    </Container>
     </div>
   );
 }
