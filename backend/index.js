6b2eec90c7c1c1f03093736f4898695925ab48be
Linda <shecodeshsucodes18@gmail.com>
2021-05-08 20:08:00 +0800

    saved with local prettier, removed duplicated get books/:letter

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,8 @@ const express = require("express"),
     morgan = require("morgan"),
     app = express(),
     PORT = parseInt(process.env.PORT) || 3010,
-    cors = require("cors")
-    mongoose = require("mongoose");
-
-
+    cors = require("cors");
+mongoose = require("mongoose");
 
 //connection
 mongoose.connect(
@@ -17,11 +15,11 @@ mongoose.connect(
         useNewUrlParser: true,
         useUnifiedTopology: true,
     },
-    (e)=>{
-        if(e) throw e;
+    (e) => {
+        if (e) throw e;
         console.log("Mongodb connected!");
     }
-)
+);
 
 app.use(morgan("combined"));
 app.use(cors());
@@ -29,7 +27,7 @@ app.use(express.json());
 
 //mongoose profile schema
 const bookSchema = new mongoose.Schema({
-    book_id:{
+    book_id: {
         type: String,
         unique: true,
     },
@@ -37,124 +35,105 @@ const bookSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    authors:{
+    authors: {
         type: String,
     },
-    description:{
+    description: {
         type: String,
     },
-    edition:{
+    edition: {
         type: String,
     },
-    format:{
+    format: {
         type: String,
     },
-    pages:{
+    pages: {
         type: Number
     },
-    rating:{
+    rating: {
         type: Number
     },
-    rating_count:{
+    rating_count: {
         type: Number
     },
-    review_count:{
+    review_count: {
         type: Number
     },
-    genres:{
+    genres: {
         type: String
     },
-    image_url:{
+    image_url: {
         type: String
     }
-})
+});
 
-const Books = mongoose.model("Books", bookSchema)
+const Books = mongoose.model("Books", bookSchema);
+
+// test
+app.get('/', (req, res) => {
+    res.status(200);
+    res.json({ Hello: "Kitty" });
+});
 
 //@route books
-app.get("/books", async(req,res)=>{
+app.get("/books", async (req, res) => {
     try {
         let books = await Books.find();
         res.status(200).send({
             count: books.length,
             books,
-        })
+        });
     } catch (e) {
         console.log(e);
         res.status(500).json({
             message: "Unable to get books"
-        })
+        });
     }
-})
+});
 
-app.get("/books/:letter", async(req,res)=>{
-    const limit=10,
+app.get("/books/:letter", async (req, res) => {
+    const limit = 10,
         skip = 10;
 
     try {
         // let booksLetter = await Books.find({"title": /^T/i});
         // let booksLetter = await Books.find({ title: { $regex: "/^" + req.params.letter + "/" }});
-        let booksLetter = await Books.find({title: new RegExp('^' + req.params.letter, 'i')});
+        let booksLetter = await Books.find({ title: new RegExp('^' + req.params.letter, 'i') });
 
-        
+
         res.status(200).send({
             letter: req.params.letter,
             count: booksLetter.length,
             booksLetter
-            
-        })
+
+        });
     } catch (e) {
         console.log(e);
         res.status(500).json({
             message: `Unable to get books from alphabet ${req.params.letter}`
-        })
+        });
     }
-})
-
-//@route books/letter
-app.get("/books/:letter", async(req,res)=>{
-    const limit=10,
-        skip = 10;
-
-    try {
-        // let booksLetter = await Books.find({"title": /^T/i});
-        // let booksLetter = await Books.find({ title: { $regex: "/^" + req.params.letter + "/" }});
-        let booksLetter = await Books.find({title: new RegExp('^' + req.params.letter, 'i')});
-
-        
-        res.status(200).send({
-            letter: req.params.letter,
-            count: booksLetter.length,
-            booksLetter
-            
-        })
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: `Unable to get books from alphabet ${req.params.letter}`
-        })
-    }
-})
+});
 
 //@route book/id
-app.get("/book/:id",async(req,res)=>{
-    
+app.get("/book/:id", async (req, res) => {
+
     try {
-        let book = await Books.find({book_id: req.params.id});
-            // regex = /[|]/g,
-            // bookResult = book[0];
+        let book = await Books.find({ book_id: req.params.id });
+        // regex = /[|]/g,
+        // bookResult = book[0];
 
-            // bookResult.authors = bookResult.authors.replace(regex,", ")
-            // bookResult.genres = bookResult.genres.replace(regex,", ")
+        // bookResult.authors = bookResult.authors.replace(regex,", ")
+        // bookResult.genres = bookResult.genres.replace(regex,", ")
 
 
-        if(!book){
-            return res.status(400).json({message:"Book not found"})
+        if (!book) {
+            return res.status(400).json({ message: "Book not found" });
         }
 
         //only take what we wanted^^
-        let bookFormat = book.map(e=>{
-            return{
+        let bookFormat = book.map(e => {
+            return {
                 bookId: e.book_id,
                 title: e.title,
                 authors: e.authors,
@@ -165,22 +144,22 @@ app.get("/book/:id",async(req,res)=>{
                 ratingCount: e.rating_count,
                 image_url: e.image_url,
                 genres: e.genres.split("|"),
-            }
-        })
+            };
+        });
         res.status(200).json({
             message: "book found!",
             book,
             bookFormat
-        })
+        });
     } catch (e) {
         console.log(e);
         res.status(500).json({
             message: `Unable to get book details`
-        })
+        });
     }
-})
+});
 
-app.listen(PORT, (e)=>{
-    if (e) console.log("Error in server setup")
+app.listen(PORT, (e) => {
+    if (e) console.log("Error in server setup");
     console.log(`App is listening on PORT ${PORT} at ${new Date()}`);
-})
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import {Button, Container, Row} from 'react-bootstrap';
+import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
+	const lettersAndNumerics = "abcdefghijklmnopqrstuvwxyz0123456789",
+		letters: String[] = lettersAndNumerics.split("");
 
-  const lettersAndNumerics = "abcdefghijklmnopqrstuvwxyz0123456789",
-        letters:String[] = lettersAndNumerics.split("");
-
-  return (
-    <div className="App">
-      <Container>
-        <h1>HonnoMushi</h1>
-        <Row>
-          {letters.map((letter,index)=>(
-            <Button key={index} className="col-2 m-4 p-3" variant="outline-success">{letter}</Button>
-          ))}
-        </Row>
-    </Container>
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<Container>
+				<h1>HonnoMushi Test</h1>
+				<Row>
+					{letters.map((letter, index) => (
+						<Button
+							key={index}
+							className='col-2 m-4 p-3'
+							variant='outline-success'
+						>
+							{letter}
+						</Button>
+					))}
+				</Row>
+			</Container>
+		</div>
+	);
+};
 
 export default App;
