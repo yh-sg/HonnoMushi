3c9e13224f2067e32ce42059894e814bbca59d4e
Linda <shecodeshsucodes18@gmail.com>
2021-06-07 09:01:01 +0800

    stuck at test useParams

--- frontend/src/containers/Books/Books.test.tsx
+++ frontend/src/containers/Books/Books.test.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import ReactRouter from "react-router";
+import{ Route, MemoryRouter} from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
 import { renderWithRedux, renderWithRouter } from "../../TestingUtil";
 import Books from "./Books";
 
@@ -8,11 +12,23 @@ afterEach((): void => {
 	cleanup();
 });
 
-// const initialState = {};
+const initialState = {};
 
 describe("should render Books component correctly", () => {
 	test("renders Book component with Redux", () => {
-		jest.spyOn(ReactRouter, "useParams").mockReturnValue({ id: "/books" });
+	const configureMockStore = configureStore();
+
+	const store = configureMockStore(initialState);
+
+    render(
+      <Provider store={store}>
+    <MemoryRouter initialEntries={['books/p']}>
+      <Route path='books/:alphabet'>
+        {<Books />}
+      </Route>
+    </MemoryRouter>
+    </Provider>)
+		// jest.spyOn(ReactRouter, "useParams").mockReturnValue({ id: "/books/k" });
 		// const { getByTestId } = renderWithRouter(<Books />, initialState);
 	});
 });
