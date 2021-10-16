6a6385076fafdb414a90007af66e161be0965865
yonghan <wangyh.92@gmail.com>
2021-10-17 05:29:55 +0800

    changed the hardcoded back to process

--- backend/config/dbConfig.ts
+++ backend/config/dbConfig.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3010
 
 const connection = (app:Express) => mongoose.connect(
-    'mongodb://localhost/honnomushi',
+    process.env.MOGODBCLOUD as string,
     {
         useCreateIndex: true,
         useNewUrlParser: true,
@@ -17,8 +17,6 @@ const connection = (app:Express) => mongoose.connect(
         app.listen(PORT, async() => {
             console.log(`App is listening on PORT ${PORT} at ${new Date()}`);
             console.log("Mongodb connected!");
-            console.log('mongodb://localhost/honnomushi');
-            
         });
 }).catch((e:Error) => console.error(e));
 
